2f55c1842c8687e3abc0a5b8ce4c4d41586cfb5a
anishdeena <anishdeena@gmail.com>
2014-06-05 00:36:23 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Livesnip::Application.config.secret_token = '8d9d7909052ef3ec913c40a3ba7062c8e83494270a07af5b5805842d3f9e9fbb4a8ff475855bc7b9389c249622fe2d0428ee5807905d805dd4745fae3c618267'
